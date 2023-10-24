35b56da42f4386fcc795b3b55e47a1dd4ce31867
Denis <den.sautner@mail.ru>
2023-10-24 15:08:58 +0600

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import NavMenu from './components/NavMenu/NavMenu.js';
-import Sidebar from './components/Sidebar/Sidebar.js';
-import AudioPlayer from './components/AudioPlayer/AudioPlayer.js';
-import Centerblock from './components/Centerblock/Centerblock.js';
+import './App.css'
+import NavMenu from './components/NavMenu/NavMenu.js'
+import Sidebar from './components/Sidebar/Sidebar.js'
+import AudioPlayer from './components/AudioPlayer/AudioPlayer.js'
+import Centerblock from './components/Centerblock/Centerblock.js'
 
 function App() {
-  return (
-    <div className="wrapper">
-      <div className="container">
-        <main className="main">
-          {NavMenu()}
-          {Centerblock()}
-          {Sidebar()}
-        </main>
-        {AudioPlayer()}
-        <footer className="footer"></footer>
-      </div>
-    </div>
-  )
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <main className="main">
+                    {NavMenu()}
+                    {Centerblock()}
+                    {Sidebar()}
+                </main>
+                {AudioPlayer()}
+                <footer className="footer"></footer>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
