e509f38e78265e00474b89300373696d6ca706e5
Denis <den.sautner@mail.ru>
2023-10-24 23:15:07 +0600

    finished skeleton at Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,20 @@
 import './Sidebar.css'
+import { useEffect, useState } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-const sidebar = () => {
+const Sidebar = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 5000)
+    }, [])
+
     return (
         <div className="main__sidebar sidebar">
             <div className="sidebar__personal">
-                <p className="sidebar__personal-name">Sergey.Ivanov</p>
+                <p className="sidebar__personal-name">Denis Sautner</p>
                 <div className="sidebar__icon">
                     <svg alt="logout">
                         <use xlinkHref="img/icon/sprite.svg#logout"></use>
@@ -15,29 +25,56 @@ const sidebar = () => {
                 <div className="sidebar__list">
                     <div className="sidebar__item">
                         <a className="sidebar__link" href="#123">
-                            <img
-                                className="sidebar__img"
-                                src="img/playlist01.png"
-                                alt="day's playlist"
-                            />
+                            {isLoading ? (
+                                <Skeleton
+                                    width={250}
+                                    height={150}
+                                    baseColor="#202020"
+                                    highlightColor="#444"
+                                />
+                            ) : (
+                                <img
+                                    className="sidebar__img"
+                                    src="img/playlist01.png"
+                                    alt="day's playlist"
+                                />
+                            )}
                         </a>
                     </div>
                     <div className="sidebar__item">
                         <a className="sidebar__link" href="#123">
-                            <img
-                                className="sidebar__img"
-                                src="img/playlist02.png"
-                                alt="day's playlist"
-                            />
+                            {isLoading ? (
+                                <Skeleton
+                                    width={250}
+                                    height={150}
+                                    baseColor="#202020"
+                                    highlightColor="#444"
+                                />
+                            ) : (
+                                <img
+                                    className="sidebar__img"
+                                    src="img/playlist02.png"
+                                    alt="day's playlist"
+                                />
+                            )}
                         </a>
                     </div>
                     <div className="sidebar__item">
                         <a className="sidebar__link" href="#123">
-                            <img
-                                className="sidebar__img"
-                                src="img/playlist03.png"
-                                alt="day's playlist"
-                            />
+                            {isLoading ? (
+                                <Skeleton
+                                    width={250}
+                                    height={150}
+                                    baseColor="#202020"
+                                    highlightColor="#444"
+                                />
+                            ) : (
+                                <img
+                                    className="sidebar__img"
+                                    src="img/playlist03.png"
+                                    alt="day's playlist"
+                                />
+                            )}
                         </a>
                     </div>
                 </div>
@@ -46,4 +83,4 @@ const sidebar = () => {
     )
 }
 
-export default sidebar
+export default Sidebar
