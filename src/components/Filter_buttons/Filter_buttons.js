f642848abb93342e26566e5d28db0104e9107806
Denis <den.sautner@mail.ru>
2023-10-25 20:58:46 +0600

    questions on popups

--- src/components/Filter_buttons/Filter_buttons.js
+++ src/components/Filter_buttons/Filter_buttons.js
@@ -1,16 +1,54 @@
 import './Filter_buttons.css'
+import { useState } from 'react'
+import { AuthorList } from '../Filter_lists/Author_list.js'
+import { YearList } from '../Filter_lists/Year_list.js'
+import { GenreList } from '../Filter_lists/Genre_list.js'
 
 export const Filter_buttons = () => {
+    const [visibleAuthor, setVisibleAuthor] = useState(false)
+    const [visibleYear, setVisibleYear] = useState(false)
+    const [visibleGenre, setVisibleGenre] = useState(false)
+
+    const clickOnAuthorFilter = () => {
+        setVisibleAuthor(!visibleAuthor)
+        setVisibleYear(false)
+        setVisibleGenre(false)
+    }
+    const clickOnYearFilter = () => {
+        setVisibleYear(!visibleYear)
+        setVisibleAuthor(false)
+        setVisibleGenre(false)
+    }
+    const clickOnGenreFilter = () => {
+        setVisibleGenre(!visibleGenre)
+        setVisibleAuthor(false)
+        setVisibleYear(false)
+    }
+
     return (
         <div className="centerblock__filter filter">
             <div className="filter__title">Искать по:</div>
-            <div className="filter__button button-author _btn-text">
-                <div>исполнителю</div>
+            <div
+                className="filter__button button-author _btn-text"
+                onClick={clickOnAuthorFilter}
+            >
+                исполнителю
             </div>
-            <div className="filter__button button-year _btn-text">
+            {visibleAuthor && AuthorList()}
+            <div
+                className="filter__button button-year _btn-text"
+                onClick={clickOnYearFilter}
+            >
                 году выпуска
             </div>
-            <div className="filter__button button-genre _btn-text">жанру</div>
+            {visibleYear && YearList()}
+            <div
+                className="filter__button button-genre _btn-text"
+                onClick={clickOnGenreFilter}
+            >
+                жанру
+            </div>
+            {visibleGenre && GenreList()}
         </div>
     )
 }
