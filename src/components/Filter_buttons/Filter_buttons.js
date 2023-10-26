9fbb0d265e55313dda3c3fbd711e5d30b83a08a4
Denis <den.sautner@mail.ru>
2023-10-26 13:03:03 +0600

    finished filters

--- src/components/Filter_buttons/Filter_buttons.js
+++ src/components/Filter_buttons/Filter_buttons.js
@@ -5,21 +5,34 @@ import { YearList } from '../Filter_lists/Year_list.js'
 import { GenreList } from '../Filter_lists/Genre_list.js'
 
 export const Filter_buttons = () => {
+    const [activeAuthor, setActiveAuthor] = useState(false)
+    const [activeYear, setActiveYear] = useState(false)
+    const [activeGenre, setActiveGenre] = useState(false)
+
     const [visibleAuthor, setVisibleAuthor] = useState(false)
     const [visibleYear, setVisibleYear] = useState(false)
     const [visibleGenre, setVisibleGenre] = useState(false)
 
     const clickOnAuthorFilter = () => {
+        setActiveAuthor(!activeAuthor)
+        setActiveYear(false)
+        setActiveGenre(false)
         setVisibleAuthor(!visibleAuthor)
         setVisibleYear(false)
         setVisibleGenre(false)
     }
     const clickOnYearFilter = () => {
+        setActiveAuthor(false)
+        setActiveYear(!activeYear)
+        setActiveGenre(false)
         setVisibleYear(!visibleYear)
         setVisibleAuthor(false)
         setVisibleGenre(false)
     }
     const clickOnGenreFilter = () => {
+        setActiveAuthor(false)
+        setActiveYear(false)
+        setActiveGenre(!activeGenre)
         setVisibleGenre(!visibleGenre)
         setVisibleAuthor(false)
         setVisibleYear(false)
@@ -28,27 +41,39 @@ export const Filter_buttons = () => {
     return (
         <div className="centerblock__filter filter">
             <div className="filter__title">Искать по:</div>
-            <div
-                className="filter__button button-author _btn-text"
-                onClick={clickOnAuthorFilter}
-            >
-                исполнителю
+            <div className="filter__content">
+                <div
+                    className={`filter__button button-author _btn-text ${
+                        activeAuthor ? 'active margin' : ''
+                    }`}
+                    onClick={clickOnAuthorFilter}
+                >
+                    исполнителю
+                </div>
+                {visibleAuthor && AuthorList()}
             </div>
-            {visibleAuthor && AuthorList()}
-            <div
-                className="filter__button button-year _btn-text"
-                onClick={clickOnYearFilter}
-            >
-                году выпуска
+            <div className="filter__content">
+                <div
+                    className={`filter__button button-year _btn-text ${
+                        activeYear ? 'active' : ''
+                    }`}
+                    onClick={clickOnYearFilter}
+                >
+                    году выпуска
+                </div>
+                {visibleYear && YearList()}
             </div>
-            {visibleYear && YearList()}
-            <div
-                className="filter__button button-genre _btn-text"
-                onClick={clickOnGenreFilter}
-            >
-                жанру
+            <div className="filter__content">
+                <div
+                    className={`filter__button button-genre _btn-text ${
+                        activeGenre ? 'active' : ''
+                    }`}
+                    onClick={clickOnGenreFilter}
+                >
+                    жанру
+                </div>
+                {visibleGenre && GenreList()}
             </div>
-            {visibleGenre && GenreList()}
         </div>
     )
 }
