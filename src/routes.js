00e20ff7b08d8c7b1b9fce164a94726cf08ffe61
Denis <den.sautner@mail.ru>
2023-11-10 16:25:08 +0600

    added ProtectedRoute.js

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,19 @@ import { SignUp } from './pages/SignUp/SignUp.js'
 import { NotFoundPage } from './pages/NotFound/NotFound.js'
 import { MyTracksPage } from './pages/MyTracks/MyTracksPage.js'
 import { CategoryPage } from './pages/Categories/CategoryPage.js'
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute.js'
 
-export const AppRoutes = () => {
+export const AppRoutes = ({ user }) => {
     return (
         <Routes>
-            <Route path="/" element={<MainPage />}></Route>
             <Route path="/login" element={<SignIn />}></Route>
             <Route path="/register" element={<SignUp />}></Route>
             <Route path="*" element={<NotFoundPage />}></Route>
-            <Route path="/favorites" element={<MyTracksPage />}></Route>
-            <Route path="/category/:id" element={<CategoryPage />}></Route>
+            <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
+                <Route path="/favorites" element={<MyTracksPage />}></Route>
+                <Route path="/" element={<MainPage />}></Route>
+                <Route path="/category/:id" element={<CategoryPage />}></Route>
+            </Route>
         </Routes>
     )
 }
