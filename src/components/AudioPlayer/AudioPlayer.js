2136f7040e7d666da374ffac413a80fea6f7a161
Denis <den.sautner@mail.ru>
2024-01-01 16:04:21 +0600

    added LikeButton component

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -11,16 +11,8 @@ import {
     playNextTrack,
     playPrevTrack,
     setIsShuffled,
-    setTracks,
 } from '../../store/slices.js'
-import {
-    setLike,
-    removeLike,
-    getFavTracks,
-    getAllTracks,
-    getPlaylist,
-    refreshToken,
-} from '../../Api.js'
+import { LikeButton } from './LikeButton/LikeButton.js'
 
 export const AudioPlayer = ({
     isPlayerVisible,
@@ -53,143 +45,6 @@ export const AudioPlayer = ({
     }
 
     const toggleLoop = isLooped ? handleUnloop : handleLoop
-
-    let isLiked = activeTrack?.stared_user?.some(
-        ({ username }) => username === JSON.parse(localStorage.getItem('user')),
-    )
-
-    if (!activeTrack.stared_user) {
-        isLiked = true
-    }
-
-    const handleLike = (id, categoryId) => {
-        setLike(id)
-            .then((response) => {
-                if (response.status === 401) {
-                    refreshToken()
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then((response) => {
-                            localStorage.setItem('accessToken', response.access)
-                        })
-                        .then(() => {
-                            setLike(id)
-                        })
-                } else if (response.status !== 200) {
-                    console.log('Произошла ошибка')
-                }
-            })
-            .then(() => {
-                if (playlist === 'fav') {
-                    getFavTracks()
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                } else if (playlist === 'main') {
-                    getAllTracks()
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                } else {
-                    getPlaylist(categoryId)
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                }
-            })
-    }
-
-    const handleRemoveLike = (id, categoryId) => {
-        removeLike(id)
-            .then((response) => {
-                if (response.status === 401) {
-                    refreshToken()
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then((response) => {
-                            localStorage.setItem('accessToken', response.access)
-                        })
-                        .then(() => {
-                            removeLike(id)
-                        })
-                } else if (response.status !== 200) {
-                    console.log('Произошла ошибка')
-                }
-            })
-            .then(() => {
-                if (playlist === 'fav') {
-                    getFavTracks()
-                        .then((response) => {
-                            return response.json()
-                        })
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                } else if (playlist === 'main') {
-                    getAllTracks()
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                } else {
-                    getPlaylist(categoryId)
-                        .then((tracks) => {
-                            dispatch(setTracks({ tracks }))
-                        })
-                        .then(() => {
-                            setLoadingTracksError('')
-                            setIsLoading(false)
-                        })
-                        .catch((error) => {
-                            console.log(error)
-                        })
-                }
-            })
-    }
-
-    const handleLikeClick = (id, categoryId) => {
-        isLiked ? handleRemoveLike(id, categoryId) : handleLike(id, categoryId)
-        isLiked = !isLiked
-    }
 
     return (
         isPlayerVisible && (
@@ -336,32 +191,13 @@ export const AudioPlayer = ({
                                             )}
                                         </S.TrackPlayAlbum>
                                     </S.TrackPlayContain>
-                                    <S.TrackPlayLikeDis>
-                                        <S.TrackPlayLike>
-                                            <S.TrackPlayLikeSvg
-                                                alt="like"
-                                                $isLiked={isLiked}
-                                                onClick={(event) => {
-                                                    event.stopPropagation()
-                                                    handleLikeClick(
-                                                        activeTrack.id,
-                                                    )
-                                                }}
-                                            >
-                                                <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
-                                            </S.TrackPlayLikeSvg>
-                                        </S.TrackPlayLike>
-                                        <S.TrackPlayDislike>
-                                            <S.TrackPlayDislikeSvg
-                                                alt="dislike"
-                                                onClick={
-                                                    alertFunctionIsNotReady
-                                                }
-                                            >
-                                                <use xlinkHref="/img/icon/sprite.svg#icon-dislike"></use>
-                                            </S.TrackPlayDislikeSvg>
-                                        </S.TrackPlayDislike>
-                                    </S.TrackPlayLikeDis>
+                                    <LikeButton
+                                        setIsLoading={setIsLoading}
+                                        setLoadingTracksError={
+                                            setLoadingTracksError
+                                        }
+                                        playlist={playlist}
+                                    />
                                 </S.PlayerTrackPlay>
                             </S.BarPlayer>
                             <S.BarVolumeBlock>
