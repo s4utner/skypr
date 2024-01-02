4717d2cdaa4d0365dbb8f24b2dc7f851d741051d
Denis <den.sautner@mail.ru>
2024-01-02 14:32:29 +0600

    completed search

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -2,6 +2,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import * as S from './TracklistStyles.js'
 import { Track } from '../Track/Track.js'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 const Tracklist = ({
     isLoading,
@@ -14,14 +15,28 @@ const Tracklist = ({
     searchText,
 }) => {
     const tracks = useSelector((state) => state.tracks.tracks)
+    const [filterResult, setFilterResult] = useState([])
 
-    if (searchText) {
-        const filteredTracks = tracks.filter((track) =>
-            track.name.toLowerCase().includes(searchText.toLowerCase()),
-        )
+    useEffect(() => {
+        if (searchText) {
+            const filteredTracks = tracks.filter(
+                (track) =>
+                    track.name
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase()) ||
+                    track.author
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase()) ||
+                    track.album
+                        .toLowerCase()
+                        .includes(searchText.toLowerCase()),
+            )
 
-        console.log(filteredTracks)
-    }
+            setFilterResult(filteredTracks)
+        } else {
+            setFilterResult(tracks)
+        }
+    }, [setFilterResult, searchText, tracks])
 
     return (
         <S.ContentPlaylist>
@@ -30,20 +45,35 @@ const Tracklist = ({
                     {loadingTracksError}
                 </S.LoadingTracksError>
             )}
-            {tracks.map((track) => {
-                return (
-                    <Track
-                        key={track.id}
-                        track={track}
-                        setIsPlayerVisible={setIsPlayerVisible}
-                        isLoading={isLoading}
-                        playlist={playlist}
-                        setLoadingTracksError={setLoadingTracksError}
-                        setIsLoading={setIsLoading}
-                        categoryId={categoryId}
-                    />
-                )
-            })}
+            {filterResult.length > 0
+                ? filterResult.map((track) => {
+                      return (
+                          <Track
+                              key={track.id}
+                              track={track}
+                              setIsPlayerVisible={setIsPlayerVisible}
+                              isLoading={isLoading}
+                              playlist={playlist}
+                              setLoadingTracksError={setLoadingTracksError}
+                              setIsLoading={setIsLoading}
+                              categoryId={categoryId}
+                          />
+                      )
+                  })
+                : tracks.map((track) => {
+                      return (
+                          <Track
+                              key={track.id}
+                              track={track}
+                              setIsPlayerVisible={setIsPlayerVisible}
+                              isLoading={isLoading}
+                              playlist={playlist}
+                              setLoadingTracksError={setLoadingTracksError}
+                              setIsLoading={setIsLoading}
+                              categoryId={categoryId}
+                          />
+                      )
+                  })}
         </S.ContentPlaylist>
     )
 }
